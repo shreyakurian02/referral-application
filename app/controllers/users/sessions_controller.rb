b1ebe3d188f1e876f7ec5f4c07a9934546f01234
Shreya Kurian <shreyakurian99@gmail.com>
2023-02-27 14:24:52 +0530

    Refactored Backend code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
-
-  # POST /v1/login
   def create
-    @user = User.find_by_email(user_params[:email])
-    return invalid_login_attempt unless @user
-
+    @user = User.find_by!(email: user_params[:email])
     if @user.valid_password?(user_params[:password])
       sign_in :user, @user
-      ApplicationMailer.with(from_email: "example@gmail.com",to_email: @user.email ).send_welcome_email.deliver
-      render json: @user
+      render status: :ok, json: @user
     else
       invalid_login_attempt
     end
@@ -19,16 +14,14 @@ class Users::SessionsController < Devise::SessionsController
     render :json=> {:success=>true}
   end
 
-
-    private
+  private
 
     def invalid_login_attempt
       warden.custom_failure!
-      render json: {error: 'invalid login attempt'}, status: :unprocessable_entity
+      render json: {error: 'Invalid login attempt'}, status: :unprocessable_entity
     end
 
     def user_params
        params.require(:user).permit(:email, :password)
     end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :referrals, dependent: :destroy
+
+  validates :email, presence: true, format: { with: Constants::VALID_EMAIL_REGEX }
 end
