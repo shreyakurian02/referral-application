ab2a8cff1d81b48b66850ba789c62fd6bdc64058
Shreya Kurian <63491645+shreyakurian02@users.noreply.github.com>
2023-02-27 16:49:06 +0530

    Fixed the styling (#14)

--- app/javascript/src/components/Authentication/LoginForm.jsx
+++ app/javascript/src/components/Authentication/LoginForm.jsx
@@ -39,7 +39,6 @@ const LoginForm = ({ user, authenticationOption, setAuthenticationOption }) => {
               password_confirmation: passwordConfirmation,
             },
           });
-      console.log(email);
       localStorage.setItem("user", email);
       window.location.href = "/";
     } catch (error) {
@@ -52,71 +51,90 @@ const LoginForm = ({ user, authenticationOption, setAuthenticationOption }) => {
   }, [user]);
 
   return (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex h-screen justify-center items-center border border-red-500 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <div className="sm:max-w-md lg:w-1/2 xl:w-1/3 space-y-4">
-        <Typography>LOGIN</Typography>
-        <form onSubmit={formik.handleSubmit} className="space-y-6" noValidate>
-          <div className="flex flex-col space-y-3">
-            <TextField
-              required
-              label="Email"
-              name="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-            <TextField
-              required
-              label="Password"
-              type="password"
-              name="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-            {authenticationOption === "signup" && (
+        <Box
+          sx={{
+            backgroundColor: "white",
+            padding: "15px",
+            borderRadius: "5px",
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: "bold" }}
+            fontWeight="medium"
+            color="#1976d2"
+            display="flex"
+            justifyContent="center"
+          >
+            {authenticationOption === "signup" ? "SIGN UP" : "LOGIN"}
+          </Typography>
+          <form onSubmit={formik.handleSubmit} className="space-y-6" noValidate>
+            <div className="flex flex-col space-y-3">
               <TextField
                 required
-                label="Password Confirmation"
-                name="passwordConfirmation"
-                value={formik.values.passwordConfirmation}
+                label="Email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <TextField
+                required
+                label="Password"
+                type="password"
+                name="password"
+                value={formik.values.password}
                 onChange={formik.handleChange}
                 error={
-                  formik.touched.passwordConfirmation &&
-                  Boolean(formik.errors.passwordConfirmation)
+                  formik.touched.password && Boolean(formik.errors.password)
                 }
-                helperText={
-                  formik.touched.passwordConfirmation &&
-                  formik.errors.passwordConfirmation
-                }
+                helperText={formik.touched.password && formik.errors.password}
               />
+              {authenticationOption === "signup" && (
+                <TextField
+                  required
+                  label="Password Confirmation"
+                  name="passwordConfirmation"
+                  value={formik.values.passwordConfirmation}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.passwordConfirmation &&
+                    Boolean(formik.errors.passwordConfirmation)
+                  }
+                  helperText={
+                    formik.touched.passwordConfirmation &&
+                    formik.errors.passwordConfirmation
+                  }
+                />
+              )}
+            </div>
+            {authenticationOption === "signup" ? (
+              <Button variant="contained" type="submit">
+                Submit
+              </Button>
+            ) : (
+              <>
+                <div className="flex w-full justify-end">
+                  <Button variant="text">Forgot your Password?</Button>
+                </div>
+                <div className="flex space-x-2">
+                  <Button variant="contained" type="submit">
+                    Login
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={() => setAuthenticationOption("signup")}
+                  >
+                    Sign Up
+                  </Button>
+                </div>
+              </>
             )}
-          </div>
-          {authenticationOption === "signup" ? (
-            <Button variant="contained" type="submit">
-              Submit
-            </Button>
-          ) : (
-            <>
-              <div className="flex w-full justify-end">
-                <Button variant="text">Forgot your Password?</Button>
-              </div>
-              <div className="flex space-x-2">
-                <Button variant="contained" type="submit">
-                  Login
-                </Button>
-                <Button
-                  variant="contained"
-                  onClick={() => setAuthenticationOption("signup")}
-                >
-                  Sign Up
-                </Button>
-              </div>
-            </>
-          )}
-        </form>
+          </form>
+        </Box>
       </div>
     </div>
   );
