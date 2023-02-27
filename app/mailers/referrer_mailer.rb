b9e0d1cfc5a0c1c89687f28be5d957be8558787b
Shreya Kurian <shreyakurian99@gmail.com>
2023-02-27 22:49:00 +0530

    Fixed deployment bugs

--- app/mailers/referrer_mailer.rb
+++ app/mailers/referrer_mailer.rb
@@ -1,6 +1,5 @@
 class ReferrerMailer < ApplicationMailer
   def send_welcome_email
-    binding.pry
     @link = Rails.application.secrets[:host]
     mail(to: params[:to_email], subject: 'Thanks for joining our mailing list' )
   end
